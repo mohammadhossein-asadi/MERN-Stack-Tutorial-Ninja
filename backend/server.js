81ce879efac94e8ac26972b20547dfb1619bd72c
mohammadhossein <92786003+mohammadhossein-asadi@users.noreply.github.com>
2022-06-18 01:38:32 +0430

    Create middleware in server app

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,21 @@
+require("dotenv").config();
 const express = require("express");
 
 // express app
 const app = express();
 
+// middleware
+app.use((req, res, next) => {
+  console.log(req.path, req.method);
+  next();
+});
+
+// routes
+app.get("/", (req, res) => {
+  res.json({ message: "Welcome to the app" });
+});
+
 // listen for requests
-app.listen(4000, () => {
-  console.log("listening on port 4000");
+app.listen(process.env.PORT, () => {
+  console.log("listening on port", process.env.PORT);
 });
