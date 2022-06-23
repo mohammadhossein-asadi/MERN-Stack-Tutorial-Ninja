df78413315c0d318e8096171a19ad30f0746264a
mohammadhossein <92786003+mohammadhossein-asadi@users.noreply.github.com>
2022-06-23 20:06:39 +0430

    Create connect.js file and create new function for connect to database

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 const workoutRoutes = require("./routes/workouts");
+const connectDB = require("./db/connect");
 
 // express app
 const app = express();
@@ -17,14 +18,31 @@ app.use((req, res, next) => {
 app.use("/api/workouts", workoutRoutes);
 
 // connect to db
-mongoose
-  .connect(process.env.MONGO_URI)
-  .then(() => {
+// # First method
+
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI);
+
     // listen for requests
     app.listen(process.env.PORT, () => {
       console.log("Connected to db & listening on port", process.env.PORT);
     });
-  })
-  .catch((error) => {
+  } catch (error) {
     console.log(error);
-  });
+  }
+};
+start();
+
+// # Secound method
+// mongoose
+//   .connect(process.env.MONGO_URI)
+//   .then(() => {
+//     // listen for requests
+//     app.listen(process.env.PORT, () => {
+//       console.log("Connected to db & listening on port", process.env.PORT);
+//     });
+//   })
+//   .catch((error) => {
+//     console.log(error);
+//   });
