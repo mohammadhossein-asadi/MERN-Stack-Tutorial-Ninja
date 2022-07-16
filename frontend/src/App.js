2e0c219d303c55c42493df2cabfb842bca068ed9
mohammadhossein <92786003+mohammadhossein-asadi@users.noreply.github.com>
2022-07-16 15:04:40 +0430

    Added new styles

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,12 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 
 function App() {
   return (
     <div className="App">
       <Router>
+        <Navbar />
         <div className="pages">
           <Routes>
             <Route path="/" element={<Home />} />
--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,5 +1,15 @@
+import { Link } from "react-router-dom";
+
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <header>
+      <div className="container">
+        <Link to={"/"}>
+          <h1>Workout Budy</h1>
+        </Link>
+      </div>
+    </header>
+  );
 };
 
 export default Navbar;
