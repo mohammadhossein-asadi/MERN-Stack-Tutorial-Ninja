4c103562e6c992785a94f92e60879798cec7ed4d
mohammadhossein <92786003+mohammadhossein-asadi@users.noreply.github.com>
2022-07-16 14:46:48 +0430

    Create components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,18 @@
-import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Router>
+        <div className="pages">
+          <Routes>
+            <Route path="/" element={<Home />} />
+          </Routes>
+        </div>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
